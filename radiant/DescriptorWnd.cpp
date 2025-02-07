0c43d4fff5d2c2fa8a634b0ed39c38c73bac0448
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-07 14:40:08 +0000

    add descriptors panel

---
+++ radiant/DescriptorWnd.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <limits>
+
+//globals
+int desc_WndId = 0;
+char maxpath_t[2048];
+qboolean flush_descWnd;
+
+GtkWindow * DescriptorWindow;//descriptor window
+GtkWidget * DescSearchBar;//search bar for descriptors
+GtkWidget * DescPanel;//panel to display descriptor info
+const char * DescPanelText;//panel text for descriptors
