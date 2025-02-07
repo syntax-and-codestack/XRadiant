da461261ea728f3a885688843df61bc5e4c32785
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-07 14:42:41 +0000

    changed

--- radiant/DescriptorWnd.cpp
+++ radiant/DescriptorWnd.cpp
@@ -10,3 +10,4 @@ GtkWindow * DescriptorWindow;//descriptor window
 GtkWidget * DescSearchBar;//search bar for descriptors
 GtkWidget * DescPanel;//panel to display descriptor info
 const char * DescPanelText;//panel text for descriptors
+//
